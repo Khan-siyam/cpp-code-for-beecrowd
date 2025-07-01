7ca17748f3f29a7e6da4a9fe3fc73d7625ce681f
Mohammad Siyam Hossen Khan <khansiyamcstu23@gmail.com>
2025-07-01 13:57:10 +0600

    Add files via upload

---
+++ ethanoic acid10.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    double b,c;
+    cin>>a;
+    cin>>b,c;
+    c=a/b;
+    cout<<fixed<<setprecision(3)<<c<<" km/l"<<endl;
+    return 0;
+}
